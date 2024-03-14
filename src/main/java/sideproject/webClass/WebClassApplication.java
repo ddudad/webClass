92dab97f2eba4f3a6d01f4bce959166fdf28cd6d
hap-star <donald8848@gmail.com>
2024-03-14 18:41:51 +0900

    init project

---
+++ src/main/java/sideproject/webClass/WebClassApplication.java
@@ -0,0 +1,13 @@
+package sideproject.webClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sideproject/webClass/WebClassApplicationTests.java
@@ -0,0 +1,13 @@
+package sideproject.webClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
